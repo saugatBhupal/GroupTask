8a4460eabfb72b30dac6986e9d2e4807fcf9acc7
Anukul Nepal <anukulnepal2014@gmail.com>
2022-09-01 21:40:41 +0545

    completed

--- gamebyAnukul.py
+++ gamebyAnukul.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# Guessing only the specific number between 1 to 10 with ifelse :
+guess_number = 10
+guess=int(input("Please guess the number between 1 to 10 : "))
+if guess > guess_number:
+    print("Sorry your guess was too high")
+elif guess < guess_number:
+    print("Sorry your guess was too low")    
+elif guess == guess_number:
+    print("Well done you guessed the number right!") 
